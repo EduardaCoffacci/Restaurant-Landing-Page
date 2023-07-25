8f613defbcbb4f6b9734a622b19bc3e374a7a3a3
Maria Eduarda <dudinha.coffacci@hotmail.com>
2023-07-24 23:15:56 -0300

    pagina about

---
+++ src/components/About.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import AboutBackgroun from "../assets/about-background.png";
+import AboutBackgroundImage from "../assets/about-background-image.png";
+import { BsFill0CircleFill, BsFillPlayCircleFill } from "react-icons/bs";
+
+const About = () => {
+  return (
+    <div className="about-section-container">
+      <div className="about-background-image-container">
+        <img src={AboutBackgroun} alt="" />
+      </div>
+      <div className="about-section-image-container">
+        <img src={AboutBackgroundImage} alt="" />
+      </div>
+      <div className="about-section-text-container">
+        <p className="primary-subheading">About</p>
+        <h1>Food Is An Important PArt Of A Balanced Diet</h1>
+        <p className="primary-text">
+          Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
+          elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
+        </p>
+        <p className="primary-text">
+          Non tincidunt magna non et elit. Dolor turpis molestie dui magnis
+          facilisis at fringilla quam.
+        </p>
+        <div className="about-buttons-container">
+          <button className="secondary-button">Learn More</button>
+          <button className="watch-video-button">
+            <BsFillPlayCircleFill /> Watch Video
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { Navbar } from "./navbar";
 import bannerbackground from "../assets/home-banner-background.png";
-import bannerImage from "../assets/home-banner-image.png";
+import BannerImage from "../assets/home-banner-image.png";
 import { FiArrowRight } from "react-icons/fi";
+import { Navbar } from "./Navbar";
 const Home = () => {
   return (
     <div className="home-container">
@@ -16,15 +16,15 @@ const Home = () => {
             Sua comida favorita entregue quente e fresca
           </h1>
           <p className="primary-text">
-            Chefs saudáveis ​​​​fazem todo o trabalho de preparação, como picar e marinar para que
-            você possa cozinhar uma comida fresca.
+            Chefs saudáveis ​​​​fazem todo o trabalho de preparação, como picar
+            e marinar para que você possa cozinhar uma comida fresca.
           </p>
           <button className="secondary-button">
             Peça agora <FiArrowRight />
           </button>
         </div>
-        <div className="home-image-section">
-          <img src={bannerImage} alt="" />
+        <div className="home-image-container">
+          <img src={BannerImage} alt="" />
         </div>
       </div>
     </div>
