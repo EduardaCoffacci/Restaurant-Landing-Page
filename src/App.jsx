d0aedc0b6b250129bad3b8b22599244f83f8a4af
Maria Eduarda <dudinha.coffacci@hotmail.com>
2023-07-26 16:35:19 -0300

    pagina depoimento e contato

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import  Work  from "./components/work"
+import Work from "./components/work";
 import "./App.css";
 import About from "./components/About";
-import  Home  from "./components/Home";
+import Home from "./components/Home";
+import Testimonial from "./components/Testimonial";
+import Contato from "./components/Contato";
 function App() {
-
   return (
     <div className="App">
-      <Home/>
-      <About/>
-      <Work/>
+      <Home />
+      <About />
+      <Work />
+      <Testimonial />
+      <Contato />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
-
+import React from "react";
+import ProfilePic from "../assets/john-doe-image.png";
+import { AiFillStar } from "react-icons/ai";
 const Testimonial = () => {
   return (
-    <div>
-      
+    <div className="work-section-wrapper">
+      <div className="work-section-top">
+        <p className="primary-subheading">Depoimento</p>
+        <h1 className="primary-heading h1-heading">Oque eles estão dizendo</h1>
+        <p className="primary-text">
+          Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
+          elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
+        </p>
+        <div className="testimonial-section-bottom">
+          <img src={ProfilePic} alt="" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
+            elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
+          </p>
+          <div className="testimonials-stars-container">
+            <AiFillStar/>
+            <AiFillStar/>
+            <AiFillStar/>
+            <AiFillStar/>
+            <AiFillStar/>
+          </div>
+          <h2>
+            John Doe
+          </h2>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Testimonial
+export default Testimonial;
