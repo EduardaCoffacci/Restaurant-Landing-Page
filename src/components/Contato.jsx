b097bd05bb677d086ec9fc7909488f018bcc0d01
Maria Eduarda <dudinha.coffacci@hotmail.com>
2023-07-27 16:59:26 -0300

    texto alterado

--- src/components/Contato.jsx
+++ src/components/Contato.jsx
@@ -4,7 +4,7 @@ const Contato = () => {
   return (
     <div className="contact-page-wrapper" id="contato">
       <h1 className="primary-heading">Tem uma pergunta em mente?</h1>
-      <h1 className="primary-heading">Deixe-nos ajudar You</h1>
+      <h1 className="primary-heading">Deixe-nos ajudar</h1>
       <div className="contact-form-container">
         <input type="text" placeholder="yourmail@gmail.com" />
         <button className="secondary-button">Enviar</button>
